845c9c9616c266bb70bbad5282463c546e347109
Hirano <hnaoya1120@gmail.com>
2022-12-16 17:17:33 +0900

    ログイン画面でリロードするとHome画面でheaders原因のエラーを解消

--- src/core/domain/api/home/service.ts
+++ src/core/domain/api/home/service.ts
@@ -27,6 +27,11 @@ export const getSearchedPosts = async (keyword: string) => {
 // 全ての投稿を取得
 export const getAllPosts = async () => {
     const url = apiConfig.posts.url;
-    const response = await axios.get(url, headers);
+    // importしたheadersを使用するとログイン後にエラーが出るので直書きにしている
+    const response = await axios.get(url, {
+        headers: {
+            accessToken: localStorage.getItem("accessToken") as string,
+        },
+    });
     return response;
 };
